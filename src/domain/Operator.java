26ca0ff0289a72403ffcf458bc410ae76b5224dd
nielshoiting27 <niels.hoiting@gmail.com>
2014-12-15 15:35:07 +0100

    15 dec 2014 15:35

--- src/domain/Operator.java
+++ src/domain/Operator.java
@@ -1,5 +1,10 @@
 package domain;
 
 public class Operator {
-
+	private String operatorName;
+	private String logicalOperator;
+	public Operator(String operatorName, String logicalOperator){
+		this.operatorName = operatorName;
+		this.logicalOperator = logicalOperator;
+	}
 }
--- src/domain/businessrule/RangeRule.java
+++ src/domain/businessrule/RangeRule.java
@@ -1,18 +1,56 @@
 package domain.businessrule;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-import domain.Template;
+import domain.Component;
+import domain.Operator;
 
 
 public class RangeRule implements BusinessRule{
 	private String ruleName;
 	private ArrayList<String> triggerEvents;
-	private String operator;
+	private Operator operator;
+	private Component component1;
+	private Component component2;
+	private Component component3;
 	private String errorMessage;
 	
 	
-	public RangeRule(){
+	public RangeRule(ArrayList<String> triggerEvents, String errorMessage, Component component1, Component component2, Component component3){
+		this.triggerEvents = triggerEvents;
+		this.errorMessage = errorMessage;
+		this.component1 = component1;
+		this.component2 = component2;
+		this.component3 = component3;
+	}
+	
+	public String getGeneratedRule(){
+		File file = new File("src/ruletemplate.txt");
+		BufferedReader bf = null;
+		try {
+			bf = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scanner sc = new Scanner(bf);
+		String ruleString = "";
+		while(sc.hasNext()){
+			String line = sc.nextLine();
+			ruleString += line + "\n";
+		}
+		String triggerLine
+		for(String triggerEvent : triggerEvents){
+			
+		}
+		ruleString = ruleString.replaceAll("%errormessage%", errorMessage);
 		
+		System.out.println(ruleString);
+		return null;	
 	}
 }
